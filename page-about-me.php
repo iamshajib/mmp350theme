4ba7512e3ac19f4dfdf8b92162552293391eb02e
Mohammed Shajib <mohammed.shajib@f1006-25-34156.bmcc.cuny.edu>
2015-10-22 21:07:41 -0400

    Today is 10-21

---
+++ page-about-me.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package symple_theme
+ */
+
+get_header(); ?>
+
+<h1>My name is Shajib</h1>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<div id="aboutMe" style="background-color:red">
+				MOHAMMED
+				</div>
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+				?>
+
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
